3ab57e8a66db8edd9b39efc56a8dbe706bb73b90
Marek Osek <marekosek77@gmail.com>
2023-01-25 14:40:39 +0100

    NBP script course

---
+++ Kursy NBP.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+response = requests.get('http://api.nbp.pl/api/exchangerates/tables/b/last/1/?format=json')
+
+status_code = response.status_code
+print(status_code)
+
+result = response.json()
+print(result)
+
+a=result[0]
+print(a)   
+for i in a:
+    print(i)
+
+print("Kursy walut z tabeli:",a['table'],"z dnia:",a['effectiveDate'])
+print('nazwa waluty;','symbol waluty;','kurs waluty')
+for i in a['rates']:
+    print(i['currency']+';',i['code']+';',i['mid'])
+
+
+
+
+
+# print(result[0]['rates'])
+
+
+# for i in result[0]['rates']:
+#     print(i['currency'],i['code'],i['mid'])
+
+
+
+
+
+
+
+
+
+
+
+    
